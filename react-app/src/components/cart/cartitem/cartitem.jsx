bce8c511197c2debe4c0ce60803fd77197179cfc
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2023-03-22 16:28:06 +0530

    cart menu

--- react-app/src/components/cart/cartitem/cartitem.jsx
+++ react-app/src/components/cart/cartitem/cartitem.jsx
@@ -1,6 +1,33 @@
 import "../cartitem/cartitem.scss";
+
+import { MdClose } from "react-icons/md";
+
+import pod2 from "../../../assests/earbuds-prod-4.webp";
+
 const CartItem = () => {
-    return <div>Cart item</div>;
+    return(
+        <div className="cart-products">
+            <div className="cart-product">
+                <div className="img-container">
+                     <img src={pod2} alt="" /> 
+                </div>
+                <div className="prod-details">
+                    <span className="name">Product name</span>
+                    <MdClose className="close-btn" />
+                    <div className="quantity-buttons">
+                        <span>-</span>
+                        <span>0</span>
+                        <span>+</span>
+                    </div>
+                    <div className="text">
+                        <span>3</span>
+                        <span>X</span>
+                        <span className="highlight">&#8377;4567</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default CartItem;
